544985ff0e9a9712a47515f526628783d5fe3468
정예원 <yes3427@gmail.com>
2021-11-30 20:18:21 +0900

    set: 프로젝트 환경설정

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <div>MODI</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/environment.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const API_END_POINT = process.env.REACT_APP_API_END_POINT;
