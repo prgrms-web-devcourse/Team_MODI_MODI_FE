61ac1f27926f5a8d9af888ef59553a4d63837088
정예원 <yes3427@gmail.com>
2021-12-02 00:46:56 +0900

    feat: MemberCounter 컴포넌트 구현

---
+++ src/components/MemberCounter.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { Add, Remove } from '@mui/icons-material';
+import { Button, Grid, Typography } from '@mui/material';
+import { useState } from 'react';
+
+const MemberCounter = ({ member }) => {
+  const [count, setCount] = useState(member);
+
+  const onDecrease = () => {
+    if (count > 1) {
+      setCount((prevCount) => prevCount - 1);
+    }
+  };
+
+  const onIncrease = () => {
+    if (count < 3) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+
+  return (
+    <Grid
+      container
+      sx={{
+        color: 'black',
+        boxShadow: 1,
+        borderRadius: 1,
+        alignItems: 'center',
+        textAlign: 'center',
+        p: 2,
+        width: 300,
+        height: 100,
+      }}
+    >
+      <Grid item xs={4}>
+        <Button onClick={onDecrease}>
+          <Remove />
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="h5" align="center">
+          {count}명
+        </Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Button onClick={onIncrease}>
+          <Add />
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+MemberCounter.propTypes = {
+  member: PropTypes.number.isRequired,
+};
+
+export default MemberCounter;
