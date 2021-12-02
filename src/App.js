d4fc19faf36eaeb0513fad34073fd3b2608a2bac
정예원 <39365737+Yeewon@users.noreply.github.com>
2021-12-02 18:11:46 +0900

    set: Material UI Theme 설정
    * set: @mui/styles 추가
    
    * remove: index.css 파일 삭제
    
    * set: Material UI Theme 설정
    
    * feat: Theme 테스트를 위한 Button 추가
    
    * set: Material UI Theme 수정
    
    * set: h2 폰트 사이즈 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Button, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const globalStyles = makeStyles({
+  root: {
+    height: '100vh',
+  },
+});
+
 function App() {
+  const classes = globalStyles();
+
   return (
-    <div>
-      <div>MODI</div>
+    <div className={classes.root}>
+      <Typography color="modiGray" variant="h2">
+        MODI
+      </Typography>
+      <Button color="primary" variant="contained">
+        primary
+      </Button>
+      <Button color="secondary" variant="contained">
+        secondary
+      </Button>
+      <Button color="thirdary" variant="contained">
+        thirdary
+      </Button>
     </div>
   );
 }
