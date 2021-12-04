4686b5a1a39049217717b643728b750f166501ba
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-04 16:24:17 +0900

    pr-template 테스트용 커밋

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import { Button, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-
-const globalStyles = makeStyles({
-  root: {
-    height: '100vh',
-  },
-});
-
 function App() {
-  const classes = globalStyles();
-
-  return (
-    <div className={classes.root}>
-      <Typography color="modiGray" variant="h2">
-        MODI
-      </Typography>
-      <Button color="primary" variant="contained">
-        primary
-      </Button>
-      <Button color="secondary" variant="contained">
-        secondary
-      </Button>
-      <Button color="thirdary" variant="contained">
-        thirdary
-      </Button>
-    </div>
-  );
+  return <div />;
 }
 
 export default App;
