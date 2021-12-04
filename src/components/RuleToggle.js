3730516fccff7acf50a701f6fb2e927f0d3fbd53
정예원 <yes3427@gmail.com>
2021-12-04 14:18:59 +0900

    style: 사용하지 않는 스타일 삭제

--- src/components/RuleToggle.js
+++ src/components/RuleToggle.js
@@ -23,7 +23,6 @@ const RuleToggle = ({ ruleId, ruleName, onSelectRule }) => {
         sx={{
           m: 0.5,
           cursor: 'pointer',
-          filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
         }}
         color={selected ? 'primary' : 'default'}
         key={ruleId}
