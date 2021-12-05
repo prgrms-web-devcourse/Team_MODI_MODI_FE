1c3e44a0aff304975b8cea9c8356dc26c8babb47
goumi1009 <goumi1009@gmail.com>
2021-12-06 01:34:44 +0900

    feat: 파티생성 타이틀 컴포넌트 구현

---
+++ src/components/CreatePartyTitle.js
@@ -0,0 +1,25 @@
+import { PropTypes } from 'prop-types';
+import { Typography, Box } from '@mui/material';
+import { styled } from '@mui/system';
+const CreatePartyTitle = ({ subTitle, sx = { paddingTop: '70px' } }) => {
+  return (
+    <Box sx={sx}>
+      <Typography variant="mediumB" color="text.secondary">
+        우리, 파티를 만들어 볼까요?
+      </Typography>
+      <TitleTypography variant="large">{subTitle}</TitleTypography>
+    </Box>
+  );
+};
+
+CreatePartyTitle.propTypes = {
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  sx: PropTypes.object,
+};
+
+export default CreatePartyTitle;
+
+const TitleTypography = styled(Typography)`
+  display: block;
+`;
