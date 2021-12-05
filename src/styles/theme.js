57ed7b88022ca3aad4a343cf669e8b80dec63a15
goumi1009 <goumi1009@gmail.com>
2021-12-05 23:38:04 +0900

    style: mui 테마 button 컴포넌트 border-radius 수정

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -99,6 +99,15 @@ const theme = createTheme({
         root: {
           borderRadius: '22px / 50%',
         },
+        sizeSmall: {
+          borderRadius: 16,
+        },
+        sizeMedium: {
+          borderRadius: 18,
+        },
+        sizeLarge: {
+          borderRadius: 21,
+        },
       },
     },
   },
