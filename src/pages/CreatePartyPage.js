2398953df4a7ad1eea9ba7bb72962497b5d601d6
goumi1009 <goumi1009@gmail.com>
2021-12-06 03:41:12 +0900

    feat: step1 ott서비스 선택 작업 및 step별 setting state 추가

--- src/pages/CreatePartyPage.js
+++ src/pages/CreatePartyPage.js
@@ -1,11 +1,93 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Button, Box, MobileStepper } from '@mui/material';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
 import { styled } from '@mui/system';
 import CreatePartyTitle from 'components/CreatePartyTitle';
+import OttList from 'components/OttList';
+
+const ottServices = [
+  {
+    ottId: 1,
+    ottName: '넷플릭스',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 2,
+    ottName: '왓챠',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 3,
+    ottName: '웨이브',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 4,
+    ottName: '티빙',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 5,
+    ottName: '디즈니+',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 6,
+    ottName: '라프텔',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 7,
+    ottName: '쿠팡+',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 8,
+    ottName: '아마존 프라임',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+];
 
 const CreatePartyPage = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const [newParty, setNewParty] = useState({
+    ottId: undefined,
+    ottName: '',
+    grade: '',
+    monthlyDeposit: undefined,
+    maxMemberCapacity: undefined,
+    startDate: '',
+    endDate: '',
+    mustFilled: false,
+    rules: [
+      {
+        ruleId: undefined,
+        ruleName: '',
+      },
+      {
+        ruleId: undefined,
+        ruleName: '',
+      },
+    ],
+    sharedId: '',
+    sharedPassword: '',
+  });
 
   const handleNext = () => {
     setActiveStep(prevActiveStep => prevActiveStep + 1);
@@ -20,17 +102,33 @@ const CreatePartyPage = () => {
     console.log('submit', e);
   };
 
+  const handleSelectedOtt = id => {
+    setNewParty(current => ({
+      ...current,
+      ottId: id,
+    }));
+  };
+
+  useEffect(() => console.log(newParty), [newParty]);
+
   const getStepContent = stepNumber => {
     switch (stepNumber) {
       case 0:
         return (
-          <CreatePartyTitle
-            sx={{ paddingTop: '94px' }}
-            subTitle="어떤 서비스를 함께 이용하고 싶나요?"
-          />
+          <>
+            <CreatePartyTitle
+              sx={{ paddingTop: '94px' }}
+              subTitle="어떤 서비스를 함께 이용하고 싶나요?"
+            />
+            <OttList ottServices={ottServices} onClick={handleSelectedOtt} />
+          </>
         );
       case 1:
-        return <CreatePartyTitle subTitle="얼마 동안 함께 이용하고 싶나요?" />;
+        return (
+          <>
+            <CreatePartyTitle subTitle="얼마 동안 함께 이용하고 싶나요?" />
+          </>
+        );
       case 2:
         return (
           <CreatePartyTitle subTitle="이 파티의 규칙은 어떻게 지정할까요?" />
