0daba23949a4e0d9e48b7ade6ca2d1b2cff6f425
goumi1009 <goumi1009@gmail.com>
2021-12-06 18:08:33 +0900

    feat: PageHeader 컴포넌트 구현

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,34 @@
+import { Avatar, Typography, Box } from '@mui/material';
+import { PropTypes } from 'prop-types';
+import { styled } from '@mui/system';
+
+const PageHeader = ({ src, alt, title, children }) => {
+  return (
+    <PageHeaderStyle>
+      <Avatar src={src} alt={alt} size={72} />
+      <Typography variant="large">{title}</Typography>
+      {children && children}
+    </PageHeaderStyle>
+  );
+};
+
+PageHeader.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+const PageHeaderStyle = styled(Box)`
+  padding: 40px 30px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  button {
+    margin-left: auto;
+  }
+`;
+
+export default PageHeader;
