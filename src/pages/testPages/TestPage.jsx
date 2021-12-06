aff723cc3e02f52c29245b4410ffe0e6f0a96479
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-06 23:13:50 +0900

    feat: PricePanel 컴포넌트 구현

--- src/pages/testPages/TestPage.jsx
+++ src/pages/testPages/TestPage.jsx
@@ -1,11 +1,16 @@
 import { CssBaseline } from '@mui/material';
-import PartyTitle from 'components/PartyTitle';
+import PartyTitle from 'components/PartyTitle/index.jsx';
 
 const TestPage = () => {
   return (
     <>
       <CssBaseline>
-        <PartyTitle ottName="넷플릭스" ottGrade="스탠다드" />
+        <PartyTitle
+          ottName="넷플릭스"
+          ottGrade="스탠다드"
+          monthlyPrice={400}
+          servicePeriod={3}
+        />
       </CssBaseline>
     </>
   );
