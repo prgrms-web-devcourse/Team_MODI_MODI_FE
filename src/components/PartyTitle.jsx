3d96a55599e7083e5b6df0c3fbfa9fe7ded2f306
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-06 21:33:40 +0900

    style: PartyTitle 기본 css 스타일 작업

--- src/components/PartyTitle.jsx
+++ src/components/PartyTitle.jsx
@@ -1,39 +1,77 @@
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import { Box } from '@mui/system';
 import PropTypes from 'prop-types';
+
+import logo from 'assets/logo-main.svg';
+import { Avatar, Typography } from '@mui/material';
+
+const ottLogos = {
+  넷플릭스: logo,
+  '디즈니 플러스': logo,
+  웨이브: logo,
+};
 
 const PartyTitle = ({ ottName }) => {
   return (
     <Box
       sx={{
         display: 'flex',
-        boxSizing: 'border-box',
+        alignItems: 'center',
         height: '80px',
-        ml: 3,
-        mr: 3,
-        border: '1px solid',
       }}
     >
+      <Avatar
+        alt={`${ottName}-logo`}
+        sx={{
+          width: 72,
+          height: 72,
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.15)',
+        }}
+        src={logo}
+      />
       <Box
         sx={{
           display: 'flex',
+          flexDirection: 'column',
+          ml: '10px',
         }}
       >
+        <Typography variant="microB" color="text.secondary">
+          디즈니 플러스
+        </Typography>
+        <Typography variant="baseB" color="text.primary">
+          스탠다드
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          ml: 'auto',
+          textAlign: 'right',
+        }}
+      >
+        <Typography variant="microB" color="text.secondary" component="div">
+          월 3,500P (2개월)
+        </Typography>
         <Box
           sx={{
-            width: '72px',
-            height: '72px',
-            border: '1px solid',
+            display: 'flex',
+            alignItems: 'flex-end',
+            verticalAlign: 'baseline',
           }}
-        />
-        <Box>
-          <div>디즈니 플러스</div>
-          <div>스탠다드</div>
+        >
+          <MonetizationOnIcon sx={{ fontSize: 16 }} color="primary" />
+          <Typography variant="micro" component="span">
+            총{' '}
+          </Typography>
+          <Typography variant="mediumB" component="span">
+            7,000
+          </Typography>
+          <Typography variant="micro" component="span">
+            {' '}
+            P
+          </Typography>
         </Box>
-      </Box>
-      <Box ml="auto">
-        <div>월 3500 P(2개월)</div>
-        <div>수수료 350P</div>
-        <div>총 7000P</div>
       </Box>
     </Box>
   );
