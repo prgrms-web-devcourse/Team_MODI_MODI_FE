5484877f9dc90a9e3b2df01ce44f2abfe1535957
정예원 <yes3427@gmail.com>
2021-12-07 18:18:43 +0900

    feat: 더미 데이터 분리

---
+++ src/constants/dummyData.js
@@ -0,0 +1,89 @@
+export const rules = [
+  {
+    ruleId: 1,
+    ruleName: '1인 1회선',
+  },
+  {
+    ruleId: 2,
+    ruleName: '1인 1기기',
+  },
+  {
+    ruleId: 3,
+    ruleName: '닉네임과 동일하게 프로필 네임 설정',
+  },
+  {
+    ruleId: 4,
+    ruleName: '개인사정 환불 불가',
+  },
+  {
+    ruleId: 5,
+    ruleName: '계정양도 불가',
+  },
+  {
+    ruleId: 6,
+    ruleName: '설정 임의 변경 불가',
+  },
+  {
+    ruleId: 7,
+    ruleName: '19세 이상',
+  },
+];
+
+export const ottServices = [
+  {
+    ottId: 1,
+    ottName: '넷플릭스',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 2,
+    ottName: '왓챠',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 3,
+    ottName: '웨이브',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 4,
+    ottName: '티빙',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 5,
+    ottName: '디즈니+',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 6,
+    ottName: '라프텔',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 7,
+    ottName: '쿠팡 플레이',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+  {
+    ottId: 8,
+    ottName: '아마존 프라임',
+    grade: '프리미엄',
+    monthlyDeposit: 10000,
+    maxMemberCapacity: 4,
+  },
+];
