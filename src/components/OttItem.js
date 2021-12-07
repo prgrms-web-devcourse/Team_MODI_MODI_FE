8bf3017f4e0919b7958a7069a5363faf9e8c9099
goumi1009 <goumi1009@gamil.com>
2021-12-07 13:56:31 +0900

    feat: step1  OTT선택

--- src/components/OttItem.js
+++ src/components/OttItem.js
@@ -1,5 +1,24 @@
 import PropTypes from 'prop-types';
 import { Avatar, Button, Typography } from '@mui/material';
+import netflix from 'assets/netflix.png';
+import watcha from 'assets/watcha.png';
+import wavve from 'assets/wavve.png';
+import tving from 'assets/tving.png';
+import disney from 'assets/disney.png';
+import laftel from 'assets/laftel.png';
+import coupangPlay from 'assets/coupang-play.png';
+import primevideo from 'assets/primevideo.png';
+
+const ottImage = {
+  1: netflix,
+  2: watcha,
+  3: wavve,
+  4: tving,
+  5: disney,
+  6: laftel,
+  7: coupangPlay,
+  8: primevideo,
+};
 
 const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
   const handleClickOtt = () => {
@@ -16,6 +35,7 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
         m: 1,
         filter: `grayscale(${selected ? 0 : 100}%)`,
       }}
+      onClick={handleClickOtt}
     >
       <Avatar
         sx={{
@@ -23,8 +43,7 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
           cursor: 'pointer',
         }}
         alt="OttName"
-        src="" // 로고이미지 추가 후 수정.
-        onClick={handleClickOtt}
+        src={ottImage[ottId]} // 로고이미지 추가 후 수정.
       />
       <Typography variant="p" align="center">
         {ottName}
