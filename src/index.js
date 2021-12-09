aefd28c738ef365e150dc38adfa8c32a9bf57109
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-09 15:26:03 +0900

    git: conflict수정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-import { ThemeProvider } from '@emotion/react';
-import theme from './styles/theme';
-//import TestPage from 'pages/TestPage';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-      {/* <TestPage /> */}
-    </ThemeProvider>
-  </React.StrictMode>,
-=======
 
 import { ThemeProvider } from '@mui/material';
 import theme from 'styles/theme';
@@ -21,6 +8,5 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>,
->>>>>>> b9b33918042f170e084cf9542c6f85c0744a7294
   document.getElementById('root'),
 );
