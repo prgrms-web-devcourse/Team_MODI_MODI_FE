6a8622c832b067fff21ec97218fd3a3bfaf9faa7
정예원 <39365737+Yeewon@users.noreply.github.com>
2021-12-09 15:35:40 +0900

    feat#MODI-127: 마이 페이지 초기 구현  (#33)
    * feat: MyPartySummary 컴포넌트 구현
    
    * feat: MyPartySummary 컴포넌트 props 추가
    
    * feat: MyPartyList 컴포넌트 구현
    
    * style: MyPartySummary 컴포넌트 marginBottom 추가
    
    * feat: 파티 클릭 이벤트 구현
    
    * feat: MyPartyTab 컴포넌트 구현
    
    * feat: MyPartyTab 컴포넌트 props 추가
    
    * feat: MyPartyTab 탭 분류 기능 구현
    
    * feat: MyPartyTab useEffect 수정
    
    * feat: MyPartyTab useEffect 수정2
    
    * feat: 파티장에 따라 요금 다르게 표시하는 기능 구현
    
    * feat: 리더 여부에 따라 props 다르게 전달하는 기능 구현
    
    * feat: 상태 저장 방식 변경
    
    * feat: 마이 페이지 구현

---
+++ src/components/MyPartyList.jsx
@@ -0,0 +1,73 @@
+import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+import MyPartySummary from 'components/MyPartySummary';
+
+const MyPartyList = ({ myParties }) => {
+  const handleClickParty = partyId => {
+    console.log(partyId);
+  };
+
+  return (
+    <Box>
+      {myParties.map(party => {
+        if (party.isLeader) {
+          const {
+            partyId,
+            ottId,
+            ottName,
+            startDate,
+            endDate,
+            isLeader,
+            monthlyReimbursement,
+          } = party;
+
+          return (
+            <MyPartySummary
+              key={partyId}
+              partyId={partyId}
+              ottId={ottId}
+              ottName={ottName}
+              startDate={startDate}
+              endDate={endDate}
+              isLeader={isLeader}
+              monthlyReimbursement={monthlyReimbursement}
+              onClickParty={handleClickParty}
+            />
+          );
+        } else {
+          const {
+            partyId,
+            ottId,
+            ottName,
+            startDate,
+            endDate,
+            isLeader,
+            monthlyFee,
+            totalFee,
+          } = party;
+
+          return (
+            <MyPartySummary
+              key={partyId}
+              partyId={partyId}
+              ottId={ottId}
+              ottName={ottName}
+              startDate={startDate}
+              endDate={endDate}
+              isLeader={isLeader}
+              monthlyFee={monthlyFee}
+              totalFee={totalFee}
+              onClickParty={handleClickParty}
+            />
+          );
+        }
+      })}
+    </Box>
+  );
+};
+
+MyPartyList.propTypes = {
+  myParties: PropTypes.array,
+};
+
+export default MyPartyList;
