cd2476af42e4cd10a95acd10b2be065e2e430b9e
Yeewon <yes3427@kookmin.ac.kr>
2021-12-11 00:26:47 +0900

    refactor: ott 이미지 Avatar를 OttLogo로 수정

--- src/components/MyParty/MyPartySummary.jsx
+++ src/components/MyParty/MyPartySummary.jsx
@@ -1,27 +1,9 @@
 import PropTypes from 'prop-types';
-import { Avatar, Box, Divider, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
-import netflix from 'assets/netflix.png';
-import watcha from 'assets/watcha.png';
-import wavve from 'assets/wavve.png';
-import tving from 'assets/tving.png';
-import disney from 'assets/disney.png';
-import laftel from 'assets/laftel.png';
-import coupangPlay from 'assets/coupang-play.png';
-import primevideo from 'assets/primevideo.png';
 import { priceToString } from 'utils/priceToString';
 import crown from 'assets/crown.png';
-
-const ottImage = {
-  1: netflix,
-  2: watcha,
-  3: wavve,
-  4: tving,
-  5: disney,
-  6: laftel,
-  7: coupangPlay,
-  8: primevideo,
-};
+import OttLogo from 'components/Ott/OttLogo';
 
 const MyPartySummary = ({
   partyId,
@@ -94,17 +76,7 @@ const MyPartySummary = ({
           marginBottom: 2,
         }}
       >
-        <Avatar
-          alt="OttName"
-          src={ottImage[ottId]}
-          sx={{
-            width: 72,
-            height: 72,
-            marginBottom: 1,
-            marginRight: 2,
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.15)',
-          }}
-        />
+        <OttLogo ottName={ottName} />
         <Box
           sx={{
             display: 'flex',
