5632e72b069d03218c2918ce3b37af83edaac8f4
goumi1009 <goumi1009@gmail.com>
2021-12-11 02:25:15 +0900

    feat: mainpage carousel 구현

--- src/components/Common/MainCarousel/CardSlide.jsx
+++ src/components/Common/MainCarousel/CardSlide.jsx
@@ -8,14 +8,21 @@ import {
 } from '@mui/material';
 import OttLogo from 'components/Ott/OttLogo';
 
-const CardSlide = ({ ottName, watingCount }) => {
+const CardSlide = ({ ottId, ottName, watingCount }) => {
+  const handleClickRecruting = () => {
+    // TODO 참여 클릭하면 해당 ott 파티 목록 보기 [링크]
+    // 여기서 링크이동을 하는게 맞을까? click 이벤트를 올려서 page에서 링크처리? 아냐 버튼이 여기있는데?
+    console.log(ottId, ottName);
+  };
+
   return (
     <Card
       sx={{
         position: 'relative',
-        width: '80%',
+        width: '100%',
         minWidth: 280,
         borderRadius: 3,
+        flexShrink: 0,
       }}
     >
       <CardContent
@@ -50,6 +57,8 @@ const CardSlide = ({ ottName, watingCount }) => {
         }}
       >
         <Button
+          ottId={ottId}
+          onClick={handleClickRecruting}
           variant="contained"
           sx={[
             {
@@ -75,8 +84,10 @@ const CardSlide = ({ ottName, watingCount }) => {
 };
 
 CardSlide.propTypes = {
+  ottId: PropTypes.number,
   ottName: PropTypes.string,
-  watingCount: PropTypes.string,
+  watingCount: PropTypes.number,
+  onClick: PropTypes.func,
 };
 
 export default CardSlide;
