23628e13f5efcadcbd4381279da3a57f11d40e59
goumi1009 <goumi1009@gmail.com>
2021-12-11 03:47:23 +0900

    refector: 메인페이지 컴포넌트 분리와 OttList 컴포넌트 사용

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,108 +1,38 @@
 import { Box, Container, Typography } from '@mui/material';
-import { styled } from '@mui/system';
-import { CardSlide } from 'components/Common';
-import OttItem from 'components/Ott/OttItem';
-import { ottServices } from 'constants/dummyData';
+import OttList from 'components/Ott/OttList';
+import MainCarousel from 'components/Main/MainCarousel';
+import MainVisual from 'components/Main/MainVisual';
+import { ottServices, waitingOtts } from 'constants/dummyData';
 
 const MainPage = () => {
   const handleClickOtt = (ottId, ottName) => {
+    // TODO ott서비스 선택시 서비스 파티 목록 페이지로 이동
     console.log(ottId, ottName);
   };
 
   return (
     <Container disableGutters>
-      <VisualBox>
-        <Typography variant="visual" color="modiGray.white" component="p">
-          세상 모든 OTT
-          <br />
-          모두의 아이디 모디
-        </Typography>
-        <Typography
-          variant="small"
-          color="modiGray.white"
-          component="p"
-          sx={{ opacity: 0.6 }}
-        >
-          안정적인 모디에서 구독료 부담없이
-          <br />
-          세상 모든 OTT 서비스를 즐겨보세요
-        </Typography>
-      </VisualBox>
-
-      {/* slider */}
+      <MainVisual />
       <Box
         sx={{
+          mt: '-85px',
           position: 'relative',
-          top: '-100px',
         }}
       >
-        <CardSlide watingCount={46} ottName="디즈니 플러스" />
-      </Box>
-
-      {/* 전체서비스 보기 */}
-      <Box sx={{ padding: '30px' }}>
-        <Typography variant="baseB" component="h2" sx={{ mb: 2 }}>
-          전체 서비스 보기
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-          }}
-        >
-          {ottServices.map(({ ottId, ottName }) => (
-            <OttItem
-              key={ottId}
-              ottId={ottId}
-              ottName={ottName}
-              onSelectOtt={handleClickOtt}
-              toggleable={false}
-            />
-          ))}
+        <MainCarousel waitingOtts={waitingOtts} slideGap={20} />
+        <Box sx={{ padding: '0 30px' }}>
+          <Typography variant="baseB" component="h2" sx={{ mb: 2 }}>
+            전체 서비스 보기
+          </Typography>
+          <OttList
+            ottServices={ottServices}
+            onSelectOtt={handleClickOtt}
+            toggleable={false}
+          />
         </Box>
       </Box>
     </Container>
   );
 };
-
-const VisualBox = styled(Box)`
-  overflow: hidden;
-  position: relative;
-  padding: 12vh 30px;
-  height: 50vh;
-  background-image: radial-gradient(
-    53.14% 58.21% at 8.64% 19.43%,
-    #c2d15d 0%,
-    #9ec238 100%
-  );
-
-  ::before {
-    content: '';
-    display: block;
-    position: absolute;
-    right: -2%;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 50vw;
-    height: 50vw;
-    border-radius: 50%;
-    background: #fff;
-    opacity: 0.1;
-  }
-  ::after {
-    content: '';
-    display: block;
-    position: absolute;
-    right: 4%;
-    top: 50%;
-    transform: translateY(-45%);
-    width: 50vw;
-    height: 50vw;
-    border-radius: 50%;
-    border: 1px dashed #fff;
-    opacity: 0.2;
-  }
-`;
 
 export default MainPage;
