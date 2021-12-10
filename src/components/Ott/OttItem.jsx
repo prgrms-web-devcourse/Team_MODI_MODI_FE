f9b3ad7392c849ea504dc40b721b6066d9968bb1
Yeewon <yes3427@kookmin.ac.kr>
2021-12-10 15:42:45 +0900

    refactor: OttItem 컴포넌트 toggleable props 추가

--- src/components/Ott/OttItem.jsx
+++ src/components/Ott/OttItem.jsx
@@ -1,9 +1,32 @@
 import PropTypes from 'prop-types';
-import { Button, Typography } from '@mui/material';
-import OttLogo from './OttLogo';
+import { Avatar, Button, Typography } from '@mui/material';
+import netflix from 'assets/netflix.png';
+import watcha from 'assets/watcha.png';
+import wavve from 'assets/wavve.png';
+import tving from 'assets/tving.png';
+import disney from 'assets/disney.png';
+import laftel from 'assets/laftel.png';
+import coupangPlay from 'assets/coupang-play.png';
+import primevideo from 'assets/primevideo.png';
 
-const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
-  console.log(ottName);
+const ottImage = {
+  1: netflix,
+  2: watcha,
+  3: wavve,
+  4: tving,
+  5: disney,
+  6: laftel,
+  7: coupangPlay,
+  8: primevideo,
+};
+
+const OttItem = ({
+  ottId,
+  ottName,
+  selected = false,
+  onSelectOtt,
+  toggleable = true,
+}) => {
   const handleClickOtt = () => {
     onSelectOtt(ottId, ottName);
   };
@@ -14,21 +37,30 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
-        filter: `grayscale(${selected ? 0 : 100}%)`,
+        opacity: toggleable ? (selected ? 1 : 0.5) : 1,
+        filter: `grayscale(${toggleable ? (selected ? 0 : 70) : 0}% )`,
         width: '25%',
         cursor: 'pointer',
         marginBottom: 4,
       }}
       onClick={handleClickOtt}
     >
-      <OttLogo ottName={ottName} />
+      <Avatar
+        alt="OttName"
+        src={ottImage[ottId]}
+        sx={{
+          width: 72,
+          height: 72,
+          marginBottom: 1,
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.15)',
+        }}
+      />
       <Typography
         variant="smallB"
         color="text.primary"
         align="center"
         component="p"
         sx={{
-          mt: 1,
           wordBreak: 'keep-all',
           height: '2em',
         }}
@@ -39,11 +71,17 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
   );
 };
 
+OttItem.defaultTypes = {
+  selected: false,
+  toggleable: true,
+};
+
 OttItem.propTypes = {
   ottId: PropTypes.number,
   ottName: PropTypes.string,
   selected: PropTypes.bool,
   onSelectOtt: PropTypes.func,
+  toggleable: PropTypes.bool,
 };
 
 export default OttItem;
