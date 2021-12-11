9c2a3f82f71b84569f4deef2be6d4396a691395e
정예원 <39365737+Yeewon@users.noreply.github.com>
2021-12-11 17:18:41 +0900

    feat#MODI-151 : OttItem 재사용 가능한 컴포넌트로 수정 (#40)
    * refactor: OttItem 컴포넌트 toggleable props 추가
    
    * refactor: 메인 페이지 OttItem 컴포넌트 적용
    
    * refactor: Avatar를 OttLogo 컴포넌트로 변경

--- src/components/Ott/OttItem.jsx
+++ src/components/Ott/OttItem.jsx
@@ -2,8 +2,13 @@ import PropTypes from 'prop-types';
 import { Button, Typography } from '@mui/material';
 import OttLogo from './OttLogo';
 
-const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
-  console.log(ottName);
+const OttItem = ({
+  ottId,
+  ottName,
+  selected = false,
+  onSelectOtt,
+  toggleable = true,
+}) => {
   const handleClickOtt = () => {
     onSelectOtt(ottId, ottName);
   };
@@ -14,7 +19,8 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
-        filter: `grayscale(${selected ? 0 : 100}%)`,
+        opacity: toggleable ? (selected ? 1 : 0.5) : 1,
+        filter: `grayscale(${toggleable ? (selected ? 0 : 70) : 0}% )`,
         width: '25%',
         cursor: 'pointer',
         marginBottom: 4,
@@ -39,11 +45,17 @@ const OttItem = ({ ottId, ottName, selected, onSelectOtt }) => {
   );
 };
 
+OttItem.defaultTypes = {
+  selected: false,
+  toggleable: true,
+};
+
 OttItem.propTypes = {
   ottId: PropTypes.number,
   ottName: PropTypes.string,
   selected: PropTypes.bool,
   onSelectOtt: PropTypes.func,
+  toggleable: PropTypes.bool,
 };
 
 export default OttItem;
