a6218fc3707012a417cd1329c75e580ac8377d3b
goumi1009 <goumi1009@gmail.com>
2021-12-13 04:54:46 +0900

    feat: OTT서비스 목록, 카드 캐로셀 데이터 api 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,45 @@
 import { Box, Container, Typography } from '@mui/material';
+import useAsync from 'hooks/useAsync';
 import OttList from 'components/Ott/OttList';
 import MainCarousel from 'components/Main/MainCarousel';
 import MainVisual from 'components/Main/MainVisual';
-import { ottServices, waitingOtts } from 'constants/dummyData';
+import { getOttList, getOttWaitings } from 'utils/api/index';
+import { useNavigate } from 'react-router-dom';
+
+const ottInfoEn = {
+  넷플릭스: {
+    ottNameEn: 'netflix',
+  },
+  왓챠: {
+    ottNameEn: 'watcha',
+  },
+  '디즈니 플러스': {
+    ottNameEn: 'disneyPlus',
+  },
+  웨이브: {
+    ottNameEn: 'wavve',
+  },
+  티빙: {
+    ottNameEn: 'tving',
+  },
+  라프텔: {
+    ottNameEn: 'laftel',
+  },
+  '쿠팡 플레이': {
+    ottNameEn: 'coupangPlay',
+  },
+  '아마존 프라임': {
+    ottNameEn: 'amazonPrime',
+  },
+};
 
 const MainPage = () => {
+  const [ottList] = useAsync(getOttList());
+  const [waitings] = useAsync(getOttWaitings());
+  const navigate = useNavigate();
+
   const handleClickOtt = (ottId, ottName) => {
-    // TODO ott서비스 선택시 서비스 파티 목록 페이지로 이동
-    console.log(ottId, ottName);
+    navigate(`recruit/${ottInfoEn[ottName].ottNameEn}`);
   };
 
   return (
@@ -19,16 +51,23 @@ const MainPage = () => {
           position: 'relative',
         }}
       >
-        <MainCarousel waitingOtts={waitingOtts} slideGap={20} />
+        {waitings.value ? (
+          <MainCarousel
+            waitingOtts={waitings.value.waitingOtts}
+            slideGap={20}
+          />
+        ) : null}
         <Box sx={{ padding: '0 30px' }}>
           <Typography variant="baseB" component="h2" sx={{ mb: 2 }}>
             전체 서비스 보기
           </Typography>
-          <OttList
-            ottServices={ottServices}
-            onSelectOtt={handleClickOtt}
-            toggleable={false}
-          />
+          {ottList.value ? (
+            <OttList
+              ottServices={ottList.value.ottServices}
+              onSelectOtt={handleClickOtt}
+              toggleable={false}
+            />
+          ) : null}
         </Box>
       </Box>
     </Container>
