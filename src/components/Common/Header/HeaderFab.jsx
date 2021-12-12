6a48d050790844d728be68e762e7ba8f8ec809e5
goumi1009 <goumi1009@gmail.com>
2021-12-13 03:04:05 +0900

    fix: 로그인 버튼 로그인페이지로 링크연결

--- src/components/Common/Header/HeaderFab.jsx
+++ src/components/Common/Header/HeaderFab.jsx
@@ -7,9 +7,9 @@ const HeaderFab = ({ user, curPage, userId }) => {
   return (
     <Fab
       disableRipple={true}
-      aria-label="login"
-      component={user ? Link : 'button'}
-      to={user ? `user/${userId}` : null}
+      aria-label={user ? 'User Page' : 'Login'}
+      component={Link}
+      to={user ? `user/${userId}` : '/login'}
       color={curPage === 'main' ? 'default' : 'secondary'}
       sx={{
         width: 28,
