197c12999404abca7d366a723d96dd1568125664
Yeewon <yes3427@kookmin.ac.kr>
2021-12-14 23:21:00 +0900

    refactor: priceToString 수정

--- src/utils/priceToString.js
+++ src/utils/priceToString.js
@@ -1,3 +1,3 @@
 export const priceToString = price => {
-  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return price && price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
