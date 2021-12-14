34d740203866ff4a320743fe65122f452f92e9d5
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-15 00:10:29 +0900

    feat: paymentPage getPartyDetail API 적용

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
 import { useNavigate } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
 import { Button, Modal, Typography } from '@mui/material';
@@ -12,33 +12,35 @@ import {
 } from 'components/Common';
 import { PaymentInfo, PointChargeAlert } from 'components/Payment';
 import PartyInfo from 'components/PartyJoin/PartyInfo';
-import { PARTY_DETAIL_DUMMY } from 'constants/mockData/parttDetailDummy';
+
+import { getPartyDetail } from 'utils/api';
+import useAsync from 'hooks/useAsync';
 
 const myPoint = 5000;
 
 const PaymentPage = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
-  const partyId = searchParams.get('partyId');
-  console.log(`partyId: ${partyId}`);
-  /** API 호출 로직
-   * const [partyDetailAPIState, fetchPartyDetailAPIState] = useAsync(getPartyDetail(`/parties/${partyId}`))
-   * const [partyDetailAPIState, fetchPartyDetailAPIState] = useAsync(post  PartyDetail(`/parties/${partyId}`))
-   *
-   *
-   */
+  const partyId = useMemo(() => searchParams.get('partyId'), [searchParams]);
+  const [open, setOpen] = useState(false);
+  const [partyDetailAPIState] = useAsync(getPartyDetail, [partyId]);
 
   const {
-    ottName,
-    grade,
-    monthlyFee,
-    period,
-    rules,
-    startDate,
-    endDate,
-    totalFee,
-  } = PARTY_DETAIL_DUMMY;
-  const [open, setOpen] = useState(!!(totalFee > myPoint));
+    isLoading: partyDetailLoading,
+    value: partyDetailValue,
+    error: partyDetailError,
+  } = partyDetailAPIState;
+
+  const {
+    ottName = '',
+    grade = '',
+    monthlyPrice = 0,
+    period = 0,
+    rules = [],
+    startDate = '',
+    endDate = '',
+    totalPrice = 0,
+  } = partyDetailValue || {};
 
   const handleClosePointChargeAlert = useCallback(() => {
     setOpen(false);
@@ -57,31 +59,37 @@ const PaymentPage = () => {
       <PageContainer>
         <PageHeader title="파티 결제" />
         <PageContents>
-          <PartyInfo
-            ottName={ottName}
-            ottGrade={grade}
-            startDate={startDate}
-            endDate={endDate}
-            period={period}
-            monthlyFee={monthlyFee}
-          />
-          <Rule rules={rules} />
-          <PaymentInfo
-            totalPrice={totalFee}
-            myPoint={myPoint}
-            onClickChargeButton={handleNavigateChargePage}
-          />
-          <Button
-            variant="contained"
-            sx={{
-              width: '100%',
-              mt: 2,
-            }}
-            size="large"
-            onClick={handleOpenPointChargeAlert}
-          >
-            <Typography variant="baseB">결제하기</Typography>
-          </Button>
+          {partyDetailLoading && <div>로딩중</div>}
+          {partyDetailError && <div>에러</div>}
+          {partyDetailValue && (
+            <>
+              <PartyInfo
+                ottName={ottName}
+                ottGrade={grade}
+                startDate={startDate}
+                endDate={endDate}
+                period={period}
+                monthlyFee={monthlyPrice}
+              />
+              <Rule rules={rules} />
+              <PaymentInfo
+                totalPrice={totalPrice}
+                myPoint={myPoint}
+                onClickChargeButton={handleNavigateChargePage}
+              />
+              <Button
+                variant="contained"
+                sx={{
+                  width: '100%',
+                  mt: 2,
+                }}
+                size="large"
+                onClick={handleOpenPointChargeAlert}
+              >
+                <Typography variant="baseB">결제하기</Typography>
+              </Button>
+            </>
+          )}
         </PageContents>
       </PageContainer>
       <Modal open={open}>
@@ -89,7 +97,7 @@ const PaymentPage = () => {
           <PointChargeAlert
             onNavigateChargePage={handleNavigateChargePage}
             onClose={handleClosePointChargeAlert}
-            paymentPoint={totalFee}
+            paymentPoint={totalPrice}
             myPoint={myPoint}
           />
         </ModalBox>
