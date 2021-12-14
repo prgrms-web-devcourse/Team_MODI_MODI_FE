23df9b8243c7da06961ce472d6559c479bffc9e1
goumi1009 <goumi1009@gmail.com>
2021-12-14 16:03:40 +0900

    feat: 전체 OTT 조회 Provider app에 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import Header from 'components/Common/Header';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router';
+import Header from 'components/Common/Header';
+import useAsync from 'hooks/useAsync';
+import { getOttList } from 'utils/api';
+import { useOttInfoDispatch, useOttInfoState } from 'contexts/OttInfoProvider';
 
 function App() {
+  const [ottInfoList] = useAsync(getOttList);
+  const { ottServices } = useOttInfoState();
+  const { onUpdate } = useOttInfoDispatch();
+
+  useEffect(() => {
+    if (ottInfoList.value && !ottServices.length) {
+      console.log(ottInfoList, ottServices);
+      onUpdate(ottInfoList.value);
+    }
+  }, [ottInfoList.value]);
+
   return (
     <>
       <Header user={false} />
