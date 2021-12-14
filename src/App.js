dee4dab4c7c0b946ff34fa65a20920de9cff3808
Kim hyunseok <dorrdorr9311@gmail.com>
2021-12-15 03:07:49 +0900

    feat: myInfo sessionStorage와 AuthContext 연동

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import Header from 'components/Common/Header';
 import { useOttInfoDispatch } from 'contexts/OttInfoProvider';
 import { useOttInfoState } from 'contexts/OttInfoProvider';
 import useAsync from 'hooks/useAsync';
-import { Outlet } from 'react-router';
-import { getOttList } from 'utils/api';
+import { Outlet, useNavigate } from 'react-router';
+import { getMyInfo, getOttList } from 'utils/api';
 import { useEffect } from 'react';
 import useStorage from 'hooks/useStorage';
-
-const OTT_INFO_KEY = 'ottInfo';
+import { OTT_INFO_KEY, TOKEN_KEY, USER_INFO_KEY } from 'constants/keys';
+import { useAuthDispatch } from 'contexts/authContext';
+import { useAuthState } from 'contexts/authContext';
 
 function App() {
+  const { isLoggedIn } = useAuthState();
   const { ottServices } = useOttInfoState();
-  const { onUpdate } = useOttInfoDispatch();
+  const { onUpdate: onUpdateOttInfo } = useOttInfoDispatch();
+  const { onUpdate: onUpdateUserInfo, onLogout } = useAuthDispatch();
+  const navigate = useNavigate();
   const [storedOttInfo, setOttInfo] = useStorage(OTT_INFO_KEY, null, 'session');
+  const [storedToken] = useStorage(TOKEN_KEY, null, 'session');
+  const [storedUserInfo, setUserInfo] = useStorage(
+    USER_INFO_KEY,
+    null,
+    'session',
+  );
 
   const [getOttListApiState, fetchOttListApiState] = useAsync(
     getOttList,
@@ -20,6 +30,20 @@ function App() {
     [],
     true,
   );
+
+  const [getMyInfoApiState, fetchGetMyInfoApiState] = useAsync(
+    getMyInfo,
+    [],
+    [],
+    true,
+  );
+
+  const {
+    isLoading: myInfoLoading,
+    value: myInfoValue,
+    error: myInfoError,
+  } = getMyInfoApiState;
+
   const {
     isLoading: ottListLoading,
     value: ottListValue,
@@ -30,22 +54,46 @@ function App() {
     if (storedOttInfo === null) {
       fetchOttListApiState();
     } else {
-      onUpdate(storedOttInfo);
+      onUpdateOttInfo(storedOttInfo);
     }
-  }, [storedOttInfo, fetchOttListApiState, onUpdate]);
+  }, [storedOttInfo, fetchOttListApiState, onUpdateOttInfo]);
 
   useEffect(() => {
     if (ottListValue) {
       setOttInfo(ottListValue);
-      onUpdate(ottListValue);
+      onUpdateOttInfo(ottListValue);
     }
-  }, [setOttInfo, ottListValue, onUpdate]);
+  }, [setOttInfo, ottListValue, onUpdateOttInfo]);
+
+  useEffect(() => {
+    if (isLoggedIn && !storedUserInfo) {
+      console.log(1);
+      fetchGetMyInfoApiState();
+    } else if (isLoggedIn && storedUserInfo) {
+      onUpdateUserInfo(storedUserInfo);
+    } else {
+      onLogout();
+    }
+  }, [
+    fetchGetMyInfoApiState,
+    onUpdateUserInfo,
+    isLoggedIn,
+    storedUserInfo,
+    onLogout,
+  ]);
+
+  useEffect(() => {
+    if (myInfoValue) {
+      setUserInfo(myInfoValue);
+      onUpdateUserInfo(myInfoValue);
+    }
+  }, [onUpdateUserInfo, myInfoValue, setUserInfo]);
 
   return (
     <>
       <Header user={false} />
-      {ottListLoading && <h1>로딩중</h1>}
-      {ottListError && <h1>에러</h1>}
+      {(ottListLoading || myInfoLoading) && <h1>로딩중</h1>}
+      {(ottListError || myInfoError) && <h1>에러</h1>}
       {ottServices.length !== 0 && <Outlet />}
     </>
   );
