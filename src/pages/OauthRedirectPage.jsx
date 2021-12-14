e3ee9d1dc5721c474a05cc0916a7af5b80fe2519
HyeonSeok Kim <80025421+dorrdorr9311@users.noreply.github.com>
2021-12-14 13:54:49 +0900

    [MODI-182] authContext 구현 및 OauthRedirectPage 적용 (#52)
    * feat: authProvider 구현
    
    * feat: OauthRedirectPage에서 로그인 처리 기능 구현

--- src/pages/OauthRedirectPage.jsx
+++ src/pages/OauthRedirectPage.jsx
@@ -1,23 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import useStorage from 'hooks/useStorage';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useAuthDispatch } from 'contexts/authContext';
+import { getMyInfo } from 'utils/api';
+import useAsync from 'hooks/useAsync';
 
 const OauthRedirectPage = () => {
+  const { onUpdate, onLogin } = useAuthDispatch();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-
   const jwtToken = searchParams.get('token');
 
-  const [storedValue, setValue] = useStorage('TOKEN', '', 'session');
+  const [, setValue] = useStorage('TOKEN', jwtToken, 'session');
+
+  const [getMyInfoAPIState, fetchGetMyInfoAPIState] = useAsync(
+    getMyInfo(),
+    [],
+    true,
+  );
+
+  const { isLoading, value, error } = getMyInfoAPIState;
+  useEffect(() => {
+    setValue(jwtToken);
+  }, [setValue, jwtToken]);
 
   useEffect(() => {
-    setValue(() => {
-      sessionStorage.removeItem('TOKEN');
+    fetchGetMyInfoAPIState();
+  }, [fetchGetMyInfoAPIState]);
 
-      return jwtToken;
-    });
-    navigate('/');
-  }, [storedValue, setValue, navigate, jwtToken]);
+  useEffect(() => {
+    if (value) {
+      onUpdate(value);
+      onLogin();
+      navigate('/');
+    }
+  }, [onLogin, onUpdate, value, navigate]);
+
+  if (isLoading) {
+    <h1>로그인중...</h1>;
+  }
+
+  if (error) {
+    <h1>에러</h1>;
+  }
 
   return (
     <>
