004cd8a8afe0a2e468e799dee25935e0066b1aa5
YUNJINIM <bee1513@gmail.com>
2021-12-14 16:24:03 +0900

    fix: Header router와 병합 작업

--- src/components/Common/Header/HeaderTabs.jsx
+++ src/components/Common/Header/HeaderTabs.jsx
@@ -1,41 +1,52 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import { Tabs } from '@mui/material';
 import theme from 'styles/theme.js';
 import HeaderTab from './HeaderTab.jsx';
 import HeaderModal from './HeaderModal.jsx';
 
-const HeaderTabs = ({ curPage }) => {
+const HeaderTabs = ({ isMainPage }) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const navigate = useNavigate();
+
   return (
     <Tabs
       value={0}
-      indicatorColor={curPage === 'main' ? 'inherit' : 'secondary'}
+      TabIndicatorProps={{
+        style: {
+          display: 'none',
+        },
+      }}
       aria-label="nav tabs"
       sx={{
         minHeight: 56,
         minWidth: 70,
         lineHeight: 1,
         padding: 0,
-        color: `${curPage === 'main' ? '#fff' : theme.palette.text.secondary}`,
+        color: `${isMainPage ? '#fff' : theme.palette.text.secondary}`,
       }}
     >
       <HeaderModal open={open} onClose={handleClose} />
-      <HeaderTab label="파티찾기" curPage={curPage} onClick={handleOpen} />
-      <HeaderTab label="파티만들기" curPage={curPage} />
+      <HeaderTab label="파티찾기" isMain={isMainPage} onClick={handleOpen} />
+      <HeaderTab
+        label="파티만들기"
+        isMainPage={isMainPage}
+        onClick={() => navigate('/create')}
+      />
     </Tabs>
   );
 };
 
 HeaderTabs.defaultProps = {
-  curPage: 'main',
+  isMainPage: false,
 };
 
 HeaderTabs.propTypes = {
-  curPage: PropTypes.string,
+  isMainPage: PropTypes.bool,
 };
 
 export default HeaderTabs;
