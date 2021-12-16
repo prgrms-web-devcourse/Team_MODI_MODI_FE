c59bd43b23e884ff72a851632cf948f5c92ba5e9
goumi1009 <goumi1009@gmail.com>
2021-12-17 03:52:30 +0900

    refactor: lottieIcon 컴포넌트 분리

---
+++ src/components/Common/LottieIcon.jsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import lottie from 'lottie-web';
+import paymentSuccess from 'assets/pay-success-lottie.json';
+import paymentFail from 'assets/pay-fail-lottie.json';
+import success from 'assets/success-lottie.json';
+import fail from 'assets/fail-lottie.json';
+import { Box } from '@mui/material';
+
+const lottieTypes = {
+  success: {
+    lottie: success,
+    loop: false,
+  },
+  paymentSuccess: {
+    lottie: paymentSuccess,
+    loop: true,
+  },
+  paymentFail: {
+    lottie: paymentFail,
+    loop: true,
+  },
+  fail: {
+    lottie: fail,
+    loop: false,
+  },
+};
+
+const LottieIcon = ({ type }) => {
+  const lottieIcon = useRef();
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: lottieIcon.current,
+      render: 'svg',
+      loop: lottieTypes[type].loop,
+      autoplay: true,
+      animationData: lottieTypes[type].lottie,
+    });
+  }, [type]);
+
+  return (
+    <Box
+      sx={{
+        height: 200,
+        mt: '-30px',
+      }}
+      ref={lottieIcon}
+    />
+  );
+};
+
+LottieIcon.propTypes = {
+  type: PropTypes.string,
+};
+
+export default LottieIcon;
