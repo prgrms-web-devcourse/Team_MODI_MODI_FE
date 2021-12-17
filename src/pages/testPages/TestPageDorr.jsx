4f527dac1e6d6b15a315d6116cb6fbac6b253907
goum <40739041+goumi1009@users.noreply.github.com>
2021-12-17 18:14:31 +0900

    feat#MODI 199 alert컴포넌트 수정 (#76)
    * feat: 공통 Alert 컴포넌트 초기 구현
    
    * feat: alert 컴포넌트 type별 lottie 이미지 적용
    
    * fix: alert modal 수정중
    
    * refactor: lottieIcon 컴포넌트 분리
    
    * refactor:  alert 컴포넌트에 모달 컴포넌트 추가해 사용성 높임

--- src/pages/testPages/TestPageDorr.jsx
+++ src/pages/testPages/TestPageDorr.jsx
@@ -1,10 +1,13 @@
 import { CssBaseline } from '@mui/material';
 import useAsync from 'hooks/useAsync';
 import { getOtt } from 'utils/api';
+import { useState } from 'react';
+import Alert from 'components/Common/Alert';
 
 const TestPageDorr = () => {
-  const [state, callback] = useAsync(getOtt);
+  const [state, callback] = useAsync(getOtt, [1], [], true);
   const { isLoading, value, error } = state;
+  const [isOpen, setIsOpen] = useState(false);
 
   console.log(isLoading, value, error);
 
@@ -25,6 +28,8 @@ const TestPageDorr = () => {
       <button onClick={() => callback(2, 5)}>불러오기</button>
       <button onClick={() => callback(5)}>불러오기</button>
       <button onClick={() => callback(4)}>불러오기</button>
+      <button onClick={() => setIsOpen(true)}> 팝업열기 </button>
+      <Alert isOpen={isOpen} type="success" onClose={() => setIsOpen(false)} />
     </>
   );
 };
