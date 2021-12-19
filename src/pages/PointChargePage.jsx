754d30eacd661539b979015a8fca0e653298c472
정예원 <39365737+Yeewon@users.noreply.github.com>
2021-12-19 22:43:33 +0900

    feat#MODI-251 : 포인트 충전 페이지 validation check 구현 및 Alert 메세지 수정 (#102)

--- src/pages/PointChargePage.jsx
+++ src/pages/PointChargePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Divider } from '@mui/material';
+import { Box, Button, TextField, Divider, Typography } from '@mui/material';
 import { PageContainer, PageContents, PageHeader } from 'components/Common';
 import InfoElement from 'components/Common/InfoElement';
 import { USER_INFO_KEY } from 'constants/keys';
@@ -11,12 +11,17 @@ import { useNavigate } from 'react-router';
 import { chargePoint } from 'utils/api';
 import { pointFormatter } from 'utils/formatting';
 
+const DAY_CHARGE_LIMIT_ERROR = 'DAY_CHARGE';
+const TOTAL_CHARGE_LIMIT_ERROR = 'TOTAL_CHARGE';
+
 const PointChargePage = () => {
   const { points } = useAuthState();
   const navigate = useNavigate();
   const { onUpdate: onUpdateUserInfo } = useAuthDispatch();
   const [chargeInput, setChargeInput] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
+  const [chargeDisabled, setChargeDisabled] = useState(true);
+  const [errorType, setErrorType] = useState('');
   const [alertType, setAlertType] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
   const [chargeState, chargeCallback] = useAsync(chargePoint, [], [], true);
@@ -24,7 +29,16 @@ const PointChargePage = () => {
   const [, setUserInfo] = useStorage(USER_INFO_KEY, null, 'session');
 
   const handleChargePoint = ({ target }) => {
-    setChargeInput(Number(target.value));
+    const inputNumber = Number(target.value);
+    if (inputNumber > 100000 || inputNumber + points > 1000000) {
+      inputNumber > 100000 && setErrorType(DAY_CHARGE_LIMIT_ERROR);
+      inputNumber + points > 1000000 && setErrorType(TOTAL_CHARGE_LIMIT_ERROR);
+      setChargeDisabled(true);
+    } else {
+      setErrorType('');
+      setChargeDisabled(false);
+    }
+    setChargeInput(inputNumber);
   };
 
   const handleChargeClick = () => {
@@ -41,7 +55,7 @@ const PointChargePage = () => {
       }));
       onUpdateUserInfo(chargeValue);
       setAlertType('paymentSuccess');
-      setAlertMessage('결제에 성공했습니다!');
+      setAlertMessage('충전에 성공했습니다!');
       setIsOpen(prev => !prev);
     }
   }, [chargeValue, onUpdateUserInfo, setUserInfo, navigate]);
@@ -49,7 +63,7 @@ const PointChargePage = () => {
   useEffect(() => {
     if (chargeError) {
       setAlertType('paymentFail');
-      setAlertMessage('결제에 실패했습니다!');
+      setAlertMessage('충전에 실패했습니다!');
       setIsOpen(prev => !prev);
     }
   }, [chargeError]);
@@ -74,6 +88,18 @@ const PointChargePage = () => {
                 mb: 6,
               }}
             />
+            {errorType === DAY_CHARGE_LIMIT_ERROR && (
+              <Typography
+                color="error"
+                sx={{
+                  mt: -5,
+                  ml: 2,
+                  mb: 2,
+                }}
+              >
+                최대 충전 금액은 100,000원 입니다.
+              </Typography>
+            )}
             <Box
               sx={{
                 mb: 3,
@@ -84,30 +110,65 @@ const PointChargePage = () => {
                   contentL: '보유 포인트',
                   variantL: 'smallB',
                 }}
-                right={{ contentR: pointFormatter(points) }}
+                right={{
+                  contentR: pointFormatter(points),
+                }}
               />
               <InfoElement
                 left={{
                   contentL: '충전 포인트',
                   variantL: 'smallB',
+                  colorL:
+                    errorType === DAY_CHARGE_LIMIT_ERROR
+                      ? 'error'
+                      : 'text.secondary',
                 }}
-                right={{ contentR: pointFormatter(chargeInput) }}
+                right={{
+                  contentR: pointFormatter(chargeInput),
+                  colorR:
+                    errorType === DAY_CHARGE_LIMIT_ERROR
+                      ? 'error'
+                      : 'text.secondary',
+                }}
               />
               <Divider />
               <InfoElement
                 left={{
                   contentL: '충전 후 예상 포인트',
                   variantL: 'smallB',
-                  colorL: 'secondary',
+                  colorL:
+                    errorType === TOTAL_CHARGE_LIMIT_ERROR
+                      ? 'error'
+                      : 'secondary',
                 }}
                 right={{
-                  contentR: pointFormatter(chargeInput + points, 25),
+                  contentR: pointFormatter(chargeInput + points, 20),
                   variantR: 'large',
-                  colorR: 'text.primary',
+                  colorR:
+                    errorType === TOTAL_CHARGE_LIMIT_ERROR
+                      ? 'error'
+                      : 'text.primary',
                 }}
               />
             </Box>
-            <Button variant="contained" onClick={handleChargeClick}>
+            {errorType === TOTAL_CHARGE_LIMIT_ERROR && (
+              <Typography
+                sx={{
+                  m: 2,
+                }}
+                align="center"
+                variant="base"
+              >
+                개인이 최대로 보유할 수 있는 포인트롤 초과했습니다. 💸
+                <br />
+                포인트 사용 후 다시 시도해주세요 😎
+              </Typography>
+            )}
+            <Button
+              variant="contained"
+              disabled={chargeDisabled}
+              onClick={handleChargeClick}
+            >
               충전하기
             </Button>
           </Box>
