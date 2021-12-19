26475daab1ef4b652dc218d5b41dc50d5163936f
Yeewon <yes3427@kookmin.ac.kr>
2021-12-20 04:33:34 +0900

    lint: 마이 페이지 오타 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -234,7 +234,6 @@ const MyPage = () => {
             onUpdateUsername={handleUpdateUsername}
             onClickShuffle={handleShuffleUsername}
           />
-          )
         </ModalBox>
       </Modal>
       <Alert
